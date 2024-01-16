c93921fde0235405cb6d9abc642f24acfaa29e18
Martina Mendizábal <marti.mendi89@gmail.com>
2024-01-16 12:55:44 -0300

    First steps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my-interactive-resume
 This is a self-maneged proyect, to get hands-on with different programming languages I am familiar with.
+To execute this interactive resume, make sure that Flask is installed in your computer. You can install it with the command `pip intall Flask`.
+Then you are able to run the app with the command `python app.py`. 
+A message should appear in your console, something similar to "Running on http://127.0.0.1:5000/", you should copy that link and paste it on your web browser but do not use Ctrl+C, use Right-click + "Copy".
---
+++ static/js/skills.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const skillsContainer = document.getElementById("skillsContainer");
+
+    // Load data from JSON file
+    fetch(new URL("../files/skills.json", import.meta.url))
+        .then(response => response.json())
+        .then(data => {
+            const skillsList = data.skills;
+            // Create skill bubbles
+            skillsList.forEach(skill => {
+                const bubblesContainer = document.createElement("div");
+                bubblesContainer.classList.add("d-grid", "gap-2")
+                const skillBubble = document.createElement("button");
+                skillBubble.classList.add("btn", "btn-outline-primary", "btn-block");
+                skillBubble.textContent = skill.name;
+                skillBubble.addEventListener("click", () => {
+                    showSkillModal(skill);
+                });
+                bubblesContainer.appendChild(skillBubble);
+                skillsContainer.appendChild(bubblesContainer);
+                
+            });
+        })
+        .catch(error => console.error("Error al cargar el archivo JSON:", error));
+});
+
+// Función para crear y mostrar el modal
+function showSkillModal(skill) {
+    const skillModal = document.createElement("div");
+    skillModal.classList.add("modal", "fade");
+    skillModal.innerHTML = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">${skill.name}</h5>
+            <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ${skill.description.map(paragraph => `<p>${paragraph}</p>`).join('')} 
+          </div>
+        </div>
+      </div>
+    `;
+  
+    // Mostrar el modal
+    const modal = new bootstrap.Modal(skillModal);
+    modal.show();
+};
